c4a0404be22d31f9854d8cd07687c9feed75621f
Cristian <c.knoxville8@gmail.com>
2020-02-26 17:29:14 -0300

    modificacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component'
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
-];
+  { path:'', redirectTo: '/dasboard', pathMatch: 'full'},
+  { path:'detail/:id', component: HeroDetailComponent},
+  { path:'heroes', component: HeroesComponent},
+  { path:'dashboard', component: DashboardComponent}
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,24 +10,32 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
   heroes:Hero[];
-  selectedHero: Hero;
-
+  
   constructor(
     private heroService: HeroService, 
     private messageService: MessageService
     ) { }
 
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+    
   getHeroes():void{
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
   }
 
-  onSelect(hero:Hero){
-    this.selectedHero = hero;
-    this.messageService.addMessage(`HeroService: Selected hero id=${hero.id}`);
-  }
-  ngOnInit(): void {
-    this.getHeroes();
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
   }
 
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 }
